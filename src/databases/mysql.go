5891f0fdadc7423bdba226b3fd10b7c217b5a0db
7947 <516948767@qq.com>
2020-07-21 08:14:35 +0800

    1.0.0.0.0

--- src/databases/mysql.go
+++ src/databases/mysql.go
@@ -9,17 +9,28 @@ import (
 var DB *xorm.Engine
 
 const (
-	DRIVER_NAME    = "mysql"
-	DATASOURCENAME = "root:toor@tcp(127.0.0.1:3306)/book?charset=utf8"
+	DRIVER_NAME        = "mysql"
+	MYSQL_NAME         = "root"
+	MYSQL_PWD          = "toor"
+	MYSQL_DEV_IP       = "127.0.0.1"
+	MYSQL_PRO_IP       = "127.0.0.1"
+	MYSQL_PORT         = "3306"
+	DATABASE_NAME      = "book"
+	CONNECTION_SEETING = "charset=utf8"
+	//DATASOURCENAME	= "root:toor@tcp(127.0.0.1:3306)/book?charset=utf8"
 )
 
 func init() {
+	sqlConnStr := MYSQL_NAME + ":" + MYSQL_PWD
+	sqlConnStr += "@tcp(" + MYSQL_DEV_IP + ":" + MYSQL_PORT + ")/"
+	sqlConnStr += DATABASE_NAME
+	sqlConnStr += "?" + CONNECTION_SEETING
 	var err error
-	DB, err = xorm.NewEngine(DRIVER_NAME, DATASOURCENAME)
+	DB, err = xorm.NewEngine(DRIVER_NAME, sqlConnStr)
 	utils.ErrorHandle(err, "faild to open mysql connection")
 
-	err = DB.Sync(new(ObjectType))
+	//err = DB.Sync(new(ObjectType))
 	utils.ErrorHandle(err, "faild to sync format table of databases")
 
-	defer DB.Close()
+	//defer DB.Close()
 }
--- src/main.go
+++ src/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	_ "gin-demo-one/src/databases"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	//fmt.Println("Hello World")
 }
