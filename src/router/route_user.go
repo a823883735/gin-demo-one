cdb097736596cb11e50b5edff3f596aae669295d
7947 <516948767@qq.com>
2020-07-24 18:37:06 +0800

    1.0.0.0.0

--- src/router/route_user.go
+++ src/router/route_user.go
@@ -1,1 +1,20 @@
 package router
+
+import (
+	"gin-demo-one/src/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var userRouter *gin.RouterGroup
+
+func init() {
+	userRouter = Router.Group("/user")
+	{
+		userRouter.GET("/", controllers.GetUsers)
+		userRouter.POST("/", controllers.AddUser)
+		userRouter.DELETE("/", controllers.DeleteUser)
+		userRouter.PUT("/", controllers.UpdataUser)
+		userRouter.GET("/findUserById", controllers.SelectUser)
+		userRouter.POST("/login", controllers.Login)
+	}
+}
